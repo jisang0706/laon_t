e604a6dabd02d835cbf1de30365a77a499409818
jisang0706 <607bit@kookmin.ac.kr>
2021-12-26 17:20:44 +0900

    Add begin setting
    1. 안드로이드 앱 초기 생성.
    2. 전국 어린이 놀이시설 API 사용법 추가
    3. 전체 UI 설계도 추가

---
+++ LaonT/app/src/main/java/com/example/laont/SecretData.kt
@@ -0,0 +1,8 @@
+package com.example.laont
+
+class SecretData {
+
+    companion object {
+        val API_serviceKey = "3SplxfYHqGy8VTWhYRjxc3%2FOHUA6cmyH4%2BROq5sYC1Tq0NiqpZ7onFfBOOw57igA%2FEa7KOheHG5TEcO%2FhSTWOg%3D%3D"
+    }
+}
